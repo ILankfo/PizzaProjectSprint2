bc3b122e5afea1848215703bab5a3ad3d90faff1
michael3244 <152321960+michael3244@users.noreply.github.com>
2023-12-03 11:36:26 -0500

    Update PizzaType.java

--- PizzaType.java
+++ PizzaType.java
@@ -1,28 +1,28 @@
 import java.util.ArrayList;
 
 
-public class PizzaType {
-    ArrayList <Pizzas> Pizzas;
+public class pizzaType {
+    ArrayList <pizzas> pizzas;
 
-    PizzaType() {
+    pizzaType() {
 
     }
-    public void AddPizza (Pizzas myPizza) {
-        Pizzas.add(myPizza);
+    public void addPizza (pizzas myPizza) {
+        pizzas.add(myPizza);
     }
-    public String getPizzaName(int PizzaID) {
-        return Pizzas.get(PizzaID).PizzaName;
+    public String getPizzaName(int pizzaID) {
+        return pizzas.get(pizzaID).pizzaName;
     }
-    public char getPizzaSize(int PizzaID) {
-        return Pizzas.get(PizzaID).PizzaSize;
+    public char getPizzaSize(int pizzaID) {
+        return pizzas.get(pizzaID).pizzaSize;
     }
-    public double getPizzaPrice(int PizzaID) {
-        return Pizzas.get(PizzaID).PizzaPrice;
+    public double getPizzaPrice(int pizzaID) {
+        return pizzas.get(pizzaID).pizzaPrice;
     }
-    public int getPizzaNum(int PizzaID) {
-        return Pizzas.get(PizzaID).PizzaNum;
+    public int getPizzaNum(int pizzaID) {
+        return pizzas.get(pizzaID).pizzaNum;
     }
-    public String getPizzaCrust(int PizzaID) {
-        return Pizzas.get(PizzaID).CrustType;
+    public String getPizzaCrust(int pizzaID) {
+        return pizzas.get(pizzaID).crustType;
     }
 }
