3257b56b2794b6d5a8927974680a27d2a74644f2
BaconLover1098 <152238302+BaconLover1098@users.noreply.github.com>
2023-12-03 15:17:12 -0500

    PizzaType.java

--- PizzaType.java
+++ PizzaType.java
@@ -28,4 +28,14 @@ public class pizzaType {
     public String getPizzaCrust(int pizzaID) {
         return pizzas.get(pizzaID).crustType;
     }
+    public void PizzaOptions() {
+        Pizzas pizza1 = new Pizzas("Cheesy Deluxe", 'L', 12.99, 1, "Cheesy Crust");
+        Pizzas.add(pizza1);
+        Pizzas pizza2 = new Pizzas("Meat Lovers", 'M', 13.99, 2, "Normal Crust");
+        Pizzas.add(pizza2);
+        Pizzas pizza3 = new Pizzas("Vegan Pizza", 'S', 8.99, 3, "Thin Crust");
+        Pizzas.add(pizza1);
+        Pizzas pizza4 = new Pizzas("Pineapple Party Pizza", 'X', 15.99, 4, "Normal Crust");
+        Pizzas.add(pizza4);
+    }
 }
