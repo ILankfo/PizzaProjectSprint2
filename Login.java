218dfebb7fc508cd1dc49fe227a7d9ea79373855
ILankfo <152224632+ILankfo@users.noreply.github.com>
2023-12-03 16:13:08 -0500

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,25 +1,61 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
-public class Login {
+public class Login extends JFrame{
+
     private static Persons myPeople = new Persons();
     private JButton login;
     private JButton cancelButton;
-    private JPasswordField passwordField1;
+    private JTextField passwordField1;
+    private JButton Login;
+    private JButton Cancel;
+    private JPasswordField passwordField2;
+    private JPanel ContentPanel;
 
 
     public Login() {
-        login.addActionListener(new ActionListener() {
+        setContentPane(ContentPanel);
+        setTitle("Login");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        Login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                onOK();
             }
         });
+        Cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+
+
+    }
+    private void onOK() {
+       if(checkPassword(passwordField1.getText(),passwordField2.getText())){
+        dispose();
+       }else {
+
+           return;
+       }
     }
 
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
     public static void main(String[] args) {
         myPeople.load();
+        new Login();
+
 
 
     }
@@ -28,11 +64,9 @@ public class Login {
         int id= myPeople.findID(email);
         if(id==-1){
             return false;
-        }else if (myPeople.getPassword(id)!=password){
+        }else if (!Objects.equals(myPeople.getPassword(id), password)){
             return false;
         }
         return true;
     }
-
-
 }
