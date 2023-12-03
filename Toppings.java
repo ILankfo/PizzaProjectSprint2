550602361e1836a56cce5a32ede8f63f4f086c49
michael3244 <152321960+michael3244@users.noreply.github.com>
2023-12-03 11:59:11 -0500

    Create Toppings.java

---
+++ Toppings.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Toppings {
+    static ArrayList <Topping> Topping;
+
+    Toppings(){}
+
+        // Creating instances of Topping
+        Topping pepperoni = new Topping("Pepperoni", 2.50);
+        Topping mushrooms = new Topping("Mushrooms", 1.75);
+        Topping onions = new Topping("Onions", 1.00);
+        Topping extraCheese = new Topping("Extra Cheese", 1.00);
+    Topping chicken = new Topping("Chicken", 2.50);
+    Topping italianSausage = new Topping("Italian Sausage", 2.15);
+    Topping olives = new Topping("Olives", 1.00);
+    Topping pineapples = new Topping("Pinapples", 1.00);
+
+
+    }
+
