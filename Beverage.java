d48057c0e596746be7a2a4dc3a1a491dec789107
SJ <152232650+Melncholy@users.noreply.github.com>
2023-12-04 10:13:07 -0500

    Update Beverage.java

--- Beverage.java
+++ Beverage.java
@@ -5,14 +5,14 @@ public class Beverage {
     double bevPrice;
     int bevNum;
     static int bevMax = 0;
-
+    ///assignment of attributes to a beverage object
     Beverage(String newBevName, char newBevSize, double newBevPrice) {
         bevName = newBevName;
         bevSize = newBevSize;
         bevPrice = newBevPrice;
         bevNum = bevMax++;
     }
-
+    ///getter for price of beverage
     public double getBevPrice() {
         return bevPrice;
     }
